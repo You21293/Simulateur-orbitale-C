24252e07c1b7c6ebf75304b444f103b518ee1b74
Younès <younes.aziz.pro@gmail.com>
2026-07-04 23:48:49 +0200

    Simulation_Satellite.c

--- simulation_satellite.c
+++ simulation_satellite.c
@@ -1,56 +1,47 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 #define G 6.67430e-11
 #define MASSE_TERRE 5.972e24
 #define RAYON_TERRE 6371000
 #define NB_SATELLITES 4
 
-typedef struct
-{
+typedef struct {
     char nom[50];
-    double altitude;
-    double masse;
-    double angle;
+    double altitude; // m
+    double masse;    // kg
+    double angle;    // degrés
 
-    double vitesse;
-    double periode;
+    double vitesse; // m/s
+    double periode; // s
+
 } Satellite;
 
 /* ========================= */
 /* CALCULS PHYSIQUES */
 /* ========================= */
 
-double calculVitesse(double altitude)
-{
+double calculVitesse(double altitude) {
     double r = RAYON_TERRE + altitude;
 
     return sqrt((G * MASSE_TERRE) / r);
 }
 
-double calculPeriode(double altitude)
-{
+double calculPeriode(double altitude) {
     double r = RAYON_TERRE + altitude;
 
-    return (2 * M_PI * r) /
-           calculVitesse(altitude);
+    return (2 * M_PI * r) / calculVitesse(altitude);
 }
 
 /* ========================= */
 /* INITIALISATION */
 /* ========================= */
 
-void initialiserSatellite(
-    Satellite *sat,
-    char nom[],
-    double altitude,
-    double masse,
-    double angle)
-{
+void initialiserSatellite(Satellite *sat, char nom[], double altitude,
+                          double masse, double angle) {
     int i = 0;
 
-    while(nom[i] != '\0')
-    {
+    while (nom[i] != '\0') {
         sat->nom[i] = nom[i];
         i++;
     }
@@ -66,197 +57,135 @@ void initialiserSatellite(
 }
 
 /* ========================= */
+/* SAISIE UTILISATEUR */
+/* ========================= */
+
+void saisirSatellite(Satellite *sat) {
+    char nom[50];
+    double altitude;
+    double masse;
+    double angle;
+
+    printf("\nNom du satellite : ");
+    scanf("%s", nom);
+
+    printf("Altitude (km) : ");
+    scanf("%lf", &altitude);
+
+    printf("Masse (kg) : ");
+    scanf("%lf", &masse);
+
+    printf("Angle initial (0-360°) : ");
+    scanf("%lf", &angle);
+
+    initialiserSatellite(sat, nom, altitude * 1000.0, masse, angle);
+}
+
+/* ========================= */
 /* AFFICHAGE */
 /* ========================= */
 
-void afficherSatellite(Satellite sat)
-{
+void afficherSatellite(Satellite sat) {
     printf("\n=============================\n");
 
     printf("Satellite : %s\n", sat.nom);
 
-    printf("Altitude : %.0f km\n",
-           sat.altitude / 1000);
+    printf("Altitude : %.0f km\n", sat.altitude / 1000);
 
-    printf("Masse : %.2f kg\n",
-           sat.masse);
+    printf("Masse : %.2f kg\n", sat.masse);
 
-    printf("Vitesse : %.2f m/s\n",
-           sat.vitesse);
+    printf("Vitesse : %.2f m/s\n", sat.vitesse);
 
-    printf("Periode : %.2f min\n",
-           sat.periode / 60.0);
+    printf("Periode : %.2f min\n", sat.periode / 60.0);
 }
 
 /* ========================= */
-/* SIMULATION ORBITALE */
+/* SIMULATION */
 /* ========================= */
 
-void simulerOrbite(Satellite sat)
-{
+void simulerOrbite(Satellite sat) {
     int temps;
 
-    printf("\n--- Simulation %s ---\n",
-           sat.nom);
+    printf("\n--- Simulation %s ---\n", sat.nom);
 
-    for(temps = 0;
-        temps <= 3600;
-        temps += 600)
-    {
-        double angle =
-        fmod(
-        (360.0 * temps) /
-        sat.periode,
-        360.0);
+    for (temps = 0; temps <= 3600; temps += 600) {
+        double angle = fmod((360.0 * temps) / sat.periode, 360.0);
 
-        printf(
-        "Temps : %4d s | Angle : %.2f degres\n",
-        temps,
-        angle);
+        printf("Temps : %4d s | Angle : %.2f degres\n", temps, angle);
     }
 }
 
 /* ========================= */
-/* DISTANCE ENTRE DEUX SAT */
+/* DISTANCE */
 /* ========================= */
 
-double distanceSatellites(
-    Satellite s1,
-    Satellite s2)
-{
-    double r1 =
-    RAYON_TERRE +
-    s1.altitude;
+double distanceSatellites(Satellite s1, Satellite s2) {
+    double r1 = RAYON_TERRE + s1.altitude;
 
-    double r2 =
-    RAYON_TERRE +
-    s2.altitude;
+    double r2 = RAYON_TERRE + s2.altitude;
 
-    double x1 =
-    r1 *
-    cos(
-    s1.angle *
-    M_PI /
-    180.0);
+    double x1 = r1 * cos(s1.angle * M_PI / 180.0);
 
-    double y1 =
-    r1 *
-    sin(
-    s1.angle *
-    M_PI /
-    180.0);
+    double y1 = r1 * sin(s1.angle * M_PI / 180.0);
 
-    double x2 =
-    r2 *
-    cos(
-    s2.angle *
-    M_PI /
-    180.0);
+    double x2 = r2 * cos(s2.angle * M_PI / 180.0);
 
-    double y2 =
-    r2 *
-    sin(
-    s2.angle *
-    M_PI /
-    180.0);
+    double y2 = r2 * sin(s2.angle * M_PI / 180.0);
 
-    return sqrt(
-    pow(x2-x1,2)
-    +
-    pow(y2-y1,2));
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
 /* ========================= */
 /* PROGRAMME PRINCIPAL */
 /* ========================= */
 
-int main()
-{
+int main() {
     Satellite flotte[NB_SATELLITES];
 
-    initialiserSatellite(
-    &flotte[0],
-    "ISS",
-    408000,
-    420000,
-    0);
+    printf("===================================\n");
+    printf(" SIMULATEUR D'ORBITE DE SATELLITES\n");
+    printf("===================================\n");
 
-    initialiserSatellite(
-    &flotte[1],
-    "Hubble",
-    540000,
-    11110,
-    90);
-
-    initialiserSatellite(
-    &flotte[2],
-    "Starlink",
-    550000,
-    260,
-    180);
-
-    initialiserSatellite(
-    &flotte[3],
-    "CubeSat",
-    400000,
-    12,
-    270);
-
-    printf(
-    "===================================\n");
-    printf(
-    " SIMULATEUR D'ORBITE DE SATELLITES\n");
-    printf(
-    "===================================\n");
+    /* Saisie des satellites */
 
     int i;
     int j;
 
-    /* Affichage des données */
+    for (i = 0; i < NB_SATELLITES; i++) {
+        printf("\n===== SATELLITE %d =====\n", i + 1);
 
-    for(i=0;i<NB_SATELLITES;i++)
-    {
-        afficherSatellite(
-        flotte[i]);
+        saisirSatellite(&flotte[i]);
+    }
+
+    /* Affichage */
+
+    printf("\n\n===== DONNEES =====\n");
+
+    for (i = 0; i < NB_SATELLITES; i++) {
+        afficherSatellite(flotte[i]);
     }
 
     /* Simulation */
 
-    printf(
-    "\n\n===== SIMULATION =====\n");
+    printf("\n\n===== SIMULATION =====\n");
 
-    for(i=0;i<NB_SATELLITES;i++)
-    {
-        simulerOrbite(
-        flotte[i]);
+    for (i = 0; i < NB_SATELLITES; i++) {
+        simulerOrbite(flotte[i]);
     }
 
-    /* Détection collisions */
+    /* Analyse des distances */
 
-    printf(
-    "\n\n===== ANALYSE DES DISTANCES =====\n");
+    printf("\n\n===== ANALYSE DES DISTANCES =====\n");
 
-    for(i=0;i<NB_SATELLITES;i++)
-    {
-        for(j=i+1;
-            j<NB_SATELLITES;
-            j++)
-        {
-            double d =
-            distanceSatellites(
-            flotte[i],
-            flotte[j]);
+    for (i = 0; i < NB_SATELLITES; i++) {
+        for (j = i + 1; j < NB_SATELLITES; j++) {
+            double d = distanceSatellites(flotte[i], flotte[j]);
 
-            printf(
-            "%s - %s : %.0f km\n",
-            flotte[i].nom,
-            flotte[j].nom,
-            d/1000);
+            printf("%s - %s : %.0f km\n", flotte[i].nom, flotte[j].nom,
+                   d / 1000);
 
-            if(d < 50000)
-            {
-                printf(
-                "ALERTE COLLISION !\n");
+            if (d < 50000) {
+                printf("ALERTE COLLISION !\n");
             }
 
             printf("\n");
